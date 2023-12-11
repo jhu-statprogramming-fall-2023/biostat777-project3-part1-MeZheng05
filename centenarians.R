a07dccdb2977e419e3cbea3470f05cf3048c0503
MeZheng05 <mzheng26@jh.edu>
2023-12-11 16:39:11 -0500

    newcommit

---
+++ centenarians.R
@@ -0,0 +1,22 @@
+#' centenarians dataset
+#'
+#' This dataset contains information about breeds.
+#'
+#' @name centenarians
+#' @format A data frame with people who aged 110 years above.
+#'\itemize{
+#'   \item rank
+#'   \item name
+#'   \item birth_date
+#'   \item death_date
+#'   \item age
+#'   \item place_of_death_or_residence
+#'   \item gender
+#'   \item still_alive
+#' }
+#' @seealso Use `data(centenarians)` to load the dataset.
+#' @examples
+#' data(centenarians)
+#'
+#' @keywords datasets
+NULL
