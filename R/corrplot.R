facf7fdff6b8e4019ce189552932589d1b01fc4f
MeZheng05 <mzheng26@jh.edu>
2023-12-11 18:13:47 -0500

    new

--- R/corrplot.R
+++ R/corrplot.R
@@ -1,3 +1,36 @@
+
+#' Data Documentation: centenarians dataset
+#'
+#' This dataset contains information about breeds.
+#'
+#' @format A data frame with people who aged 110 years above.
+#'\itemize{
+#'   \item rank
+#'   \item name
+#'   \item birth_date
+#'   \item death_date
+#'   \item age
+#'   \item place_of_death_or_residence
+#'   \item gender
+#'   \item still_alive
+#' }
+#'
+#' @keywords datasets
+#' @name centenarians
+#' @docType data
+#'
+NULL
+
+
+
+
+
+
+
+
+
+
+
 #' A visualization of a correlation matrix.
 #'
 #' A graphical display of a correlation matrix, confidence interval. The details
